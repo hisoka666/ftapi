9df6393010a0d0c1f02a9bbda8678ba2f0f23c0e
Surya Sedana <pale.deathscythe@gmail.com>
2017-02-22 12:02:12 +0800

    coba tanpa hash

--- jwt.go
+++ jwt.go
@@ -1,15 +1,19 @@
 package server
 
 import (
-   "fmt"
-   bc "golang.org/x/crypto/bcrypt"
-   "net/http"
-   "appengine"
-   "appengine/datastore"
-   _"encoding/json"
-   "io/ioutil"
-   jwt "github.com/dgrijalva/jwt-go"
-   _"time"
+	"crypto/sha256"
+	"encoding/hex"
+	_ "encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	_ "time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	bc "golang.org/x/crypto/bcrypt"
+
+	"appengine"
+	"appengine/datastore"
 )
 
 type User struct {
@@ -17,90 +21,106 @@ type User struct {
 	Password []byte
 }
 
+type UserSalt struct {
+	Username string
+	Salt     string
+}
+
+func SHA(str string) string {
+	bytes := []byte(str)
+	h := sha256.New()
+	h.Write(bytes)
+	code := h.Sum(nil)
+	codestr := hex.EncodeToString(code)
+
+	return codestr
+}
+
 //fungsi untuk registrasi user baru. fungsi ini rencananya
 //akan hanya bisa diakses oleh admin.
-func register(w http.ResponseWriter, r *http.Request){
-    ctx := appengine.NewContext(r)
+func register(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
 	username := r.FormValue("username")
 	password := r.FormValue("password")
 
 	bpass := []byte(password)
 	//salah satu metode dari paket bcrypt
-    hashed, err := bc.GenerateFromPassword(bpass, 0)
-    if err != nil {
-    	fmt.Fprintln(w, "Error %v", err)
-    }
-    stored := &User{
-    	Username: username,
-    	Password: hashed,
-    }
+	hashed, err := bc.GenerateFromPassword(bpass, 0)
+	if err != nil {
+		fmt.Fprintln(w, "Error %v", err)
+	}
+	stored := &User{
+		Username: username,
+		Password: hashed,
+	}
 
-    key := datastore.NewIncompleteKey(ctx, "User", nil)
-    if _, err := datastore.Put(ctx, key, stored); err != nil {
-    	fmt.Fprintln(w, "Failed to store %v", err)
-    }
+	key := datastore.NewIncompleteKey(ctx, "User", nil)
+	if _, err := datastore.Put(ctx, key, stored); err != nil {
+		fmt.Fprintln(w, "Failed to store %v", err)
+	}
 
 }
 
-func login(w http.ResponseWriter, r *http.Request){
+func login(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 
 	user := r.FormValue("username")
 	pass := []byte(r.FormValue("password"))
 
-    q := datastore.NewQuery("User").Filter("Username=", user)
-    t := q.Run(ctx)
-    var hashed []byte
-    for {
-    	var u User
-    	_, err := t.Next(&u)
-    	if err == datastore.Done{
-    		break
-    	}
-    	if err != nil {
-    		fmt.Fprintln(w, "Maaf username atau password salah")
-    		break
-    	}
-    	hashed = u.Password
-    }
+	q := datastore.NewQuery("User").Filter("Username=", user)
+	t := q.Run(ctx)
+	var hashed []byte
+	for {
+		var u User
+		_, err := t.Next(&u)
+		if err == datastore.Done {
+			break
+		}
+		if err != nil {
+			fmt.Fprintln(w, "Maaf username atau password salah")
+			break
+		}
+		hashed = u.Password
+	}
 
-    err := bc.CompareHashAndPassword(hashed, pass)
-    if err != nil {
-    	fmt.Fprintln(w, "Maaf username atau password salah")
-    	return
-    } else {
-    	fmt.Fprintf(w, "Selamat datang %s", user)
-    	fmt.Fprintln(w, "")
-    	signKey, err := ioutil.ReadFile("app.rsa")
-    	if err != nil {
-    		fmt.Fprintf(w, "Error %v", err)
-    		return
-    	}
-    	m := jwt.New(jwt.GetSigningMethod("RS256"))
-    	
-    	tokenString, err := m.SignedString(signKey)
-    	if err != nil {
-    		w.WriteHeader(http.StatusInternalServerError)
-    		fmt.Fprintf(w, "Error Signing Token %v", err)
-    		return
-    	}
-    	//js, err := json.Marshal(tokenString)
-    	fmt.Fprintln(w, tokenString)
+	err := bc.CompareHashAndPassword(hashed, pass)
+	if err != nil {
+		fmt.Fprintln(w, "Maaf username atau password salah")
+		return
+	} else {
+		fmt.Fprintf(w, "Selamat datang %s", user)
+		fmt.Fprintln(w, "")
+		signKey, err := ioutil.ReadFile("app.rsa")
+		if err != nil {
+			fmt.Fprintf(w, "Error %v", err)
+			return
+		}
+		fmt.Fprintln(w, signKey)
+		m := jwt.New(jwt.GetSigningMethod("RS256"))
 
-    }
+		tokenString, err := m.SignedString(signKey)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Error Signing Token %v", err)
+			return
+		}
+		//js, err := json.Marshal(tokenString)
+		fmt.Fprintln(w, tokenString)
+
+	}
 
 }
 
-func middle(next http.HandlerFunc) http.Handler{
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+func middle(next http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//mid before
 
 		next.ServeHTTP(w, r)
 		//mid after
-		})
+	})
 }
 
-func init(){
+func init() {
 	http.HandleFunc("/register", register)
 	http.HandleFunc("/login", login)
 }
